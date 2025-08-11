57fbd31b385a667860b21b0ab40a318d86af9530
Buub05 <filippospignoli@gmail.com>
2025-08-11 20:58:04 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,21 @@
-// Configurazioni principali
-const clientId = "IL_TUO_CLIENT_ID";
-const redirectUri = window.location.origin + "/index.html";
-const scopes = "playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public";
+// Configurazioni generali e costanti
+const SPOTIFY_CLIENT_ID = 'your_client_id_here';
+const SPOTIFY_REDIRECT_URI = 'your_redirect_uri_here';
+
+const SCOPES = [
+  'playlist-read-private',
+  'playlist-modify-public',
+  'playlist-modify-private',
+  'user-library-read'
+];
+
+// Config playlist
+const DEFAULT_SETTINGS = {
+  minDuration: 0,
+  maxDuration: 600,
+  genres: [],
+  languages: [],
+  mode: 'auto',
+};
+
+// Altri dati statici o impostazioni
