2580c6bf2ff6ef71275c886bb020f17f07772c5d
Buub05 <filippospignoli@gmail.com>
2025-08-11 20:36:39 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+// Configurazioni principali
+const clientId = "INSERISCI_CLIENT_ID";
+const redirectUri = window.location.origin + "/index.html";
+const scopes = "playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public";
